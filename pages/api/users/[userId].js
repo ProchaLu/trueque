cfcb26e27f0de98eecb838b567cb0ff81700df3b
Lukas Prochazka <prochazka.lu@gmail.com>
2021-11-25 13:57:41 +0100

    ts to js API change

--- pages/api/users/[userId].js
+++ pages/api/users/[userId].js
@@ -1,36 +1,10 @@
-import { NextApiRequest, NextApiResponse } from 'next';
 import {
   deleteUserById,
   getUser,
   updateUserById,
 } from '../../../util/database';
 
-export type RegisterRequest = {
-  id: number;
-  username: string;
-  name: number;
-  mail: string;
-  address: string;
-  lat: number;
-  lng: number;
-};
-
-export type User = {
-  id: number;
-  username: string;
-  name: number;
-  mail: string;
-  address: string;
-  lat: number;
-  lng: number;
-};
-
-export type RegisterResponse = { user: User };
-
-export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<RegisterResponse>,
-) {
+export default async function handler(req, res) {
   if (req.method === 'GET') {
     const user = await getUser(Number(req.query.userId));
     res.status(200).json(user);
@@ -39,7 +13,7 @@ export default async function handler(
     return res.status(200).json(deletedUser);
   } else if (req.method === 'PATCH') {
     const body = req.body;
-    const query = req.query;
+    /*  const query = req.query; */
 
     const updatedUser = await updateUserById(Number(req.query.userId), {
       name: body.name,
